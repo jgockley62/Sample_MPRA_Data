8d84b3c871e2cc07facde61e69d3b93256520b72
jgockley <jgockley62@gmail.com>
2016-04-11 14:29:22 -0400

    third commit

--- Down_Sampling/Down_Sample.pl
+++ Down_Sampling/Down_Sample.pl
@@ -8,9 +8,9 @@ use POSIX;
 
 # an array will be created where column six is the number of occurences of column 1
 #
-#USAGE: 
-#Input File: Name	Hit Sequence	Tag-Sequence	Observed_Counts Proportion_of_Total_Counts	Whole_Number_Proportion
-#Input File: synCRE_Promega_0	GCACCAGACAGTGACGTCAGCTGCCAGATCCCATGGCCGTCATACTGTGACGTCTTTCAGACACCCCATTGACGTCAATGGGAGAAC	ATCTGCGGCC	25	2.1629871745762e-07	2163
+#USAGE: perl Down_Sample.pl Rep2_mRNA.txt
+#Input File: Name       Hit Sequence    Tag-Sequence    Observed_Counts Proportion_of_Total_Counts      Whole_Number_Proportion
+#Input File: synCRE_Promega_0   GCACCAGACAGTGACGTCAGCTGCCAGATCCCATGGCCGTCATACTGTGACGTCTTTCAGACACCCCATTGACGTCAATGGGAGAAC ATCTGCGGCC      25      2.1629871745762e-07     2163
 
 my $hand1=$ARGV[0];
 
@@ -36,7 +36,7 @@ while(my $tmp = <FILE1>){
     
     #Create entry for final table
     $Final{$line[0]} = $tmp."\t0\t0\t0\t0\t0";
-    
+        
     #Add FragList Entry
     push(@FragList,$line[0]);
     
@@ -49,39 +49,24 @@ close FILE1;
 print "File Loaded\n";
 #Number of options in probability array
 my $SIZE = scalar @Permer;
+print $SIZE."\n";
+print localtime."\n";
 
-#DownSample to 75%
-for (my $i = 0; ceil(0.75*$TotalCounts); $i++){
+#DownSample to 5%
+for (my $i = 0; $i<ceil(0.05*$TotalCounts); $i++){
         
         my $Tag = $Permer[ int(rand($SIZE))-1 ];
         my @temp = split("\t", $Final{$Tag});
         #Add a read count
-        $temp[6] = $temp[6]+1;
+        $temp[10] = $temp[10]+1;    
+        #my $FOOs = join "\t", @temp;
         $Final{$Tag} = join "\t", @temp;
+
     }
-print "75% Perms Done\n";
-#DownSample to 50%
-for (my $i = 0; ceil(0.5*$TotalCounts); $i++){
-        
-        my $Tag = $Permer[ int(rand($SIZE))-1 ];
-        my @temp = split("\t", $Final{$Tag});        
-        #Add a read count
-        $temp[7] = $temp[7]+1; 
-        $Final{$Tag} = join "\t", @temp;   
-    }
-print "50% Perms Done\n";   
-#DownSample to 25%
-for (my $i = 0; ceiling(0.25*$TotalCounts); $i++){
-        
-        my $Tag = $Permer[ int(rand($SIZE))-1 ];
-        my @temp = split("\t", $Final{$Tag});        
-        #Add a read count
-        $temp[8] = $temp[8]+1;
-        $Final{$Tag} = join "\t", @temp;
-    }
-print "25% Perms Done\n";
+print "5% Perms Done\n"; 
+print localtime."\n";
 #DownSample to 10%
-for (my $i = 0; ceil(0.1*$TotalCounts); $i++){
+for (my $i = 0; $i<ceil(0.1*$TotalCounts); $i++){
         
         my $Tag = $Permer[ int(rand($SIZE))-1 ];
         my @temp = split("\t", $Final{$Tag});        
@@ -90,22 +75,48 @@ for (my $i = 0; ceil(0.1*$TotalCounts); $i++){
         $Final{$Tag} = join "\t", @temp;
     }
 print "10% Perms Done\n";
-#DownSample to 5%
-for (my $i = 0; ceil(0.5*$TotalCounts); $i++){
+print localtime."\n";
+#DownSample to 25%
+for (my $i = 0; $i<ceil(0.25*$TotalCounts); $i++){
+        
+        my $Tag = $Permer[ int(rand($SIZE))-1 ];
+        my @temp = split("\t", $Final{$Tag});        
+        #Add a read count
+        $temp[8] = $temp[8]+1;
+        $Final{$Tag} = join "\t", @temp;
+    }
+print "25% Perms Done\n";
+print localtime."\n";
+#DownSample to 50%
+for (my $i = 0; $i<ceil(0.5*$TotalCounts); $i++){
+        
+        my $Tag = $Permer[ int(rand($SIZE))-1 ];
+        my @temp = split("\t", $Final{$Tag});        
+        #Add a read count
+        $temp[7] = $temp[7]+1; 
+        $Final{$Tag} = join "\t", @temp;   
+    }
+print "50% Perms Done\n";  
+print localtime."\n";
+#DownSample to 75%
+for (my $i = 0; $i<ceil(0.75*$TotalCounts); $i++){
         
         my $Tag = $Permer[ int(rand($SIZE))-1 ];
         my @temp = split("\t", $Final{$Tag});
         #Add a read count
-        $temp[10] = $temp[10]+1;    
+        $temp[6] = $temp[6]+1;
         $Final{$Tag} = join "\t", @temp;
     }
-print "5% Perms Done\n"; 
+print "75% Perms Done\n";
+print localtime."\n"; 
+
 #####Print Out Permuted Data
 ##Create Output File
-my @Name = split(".", $hand1);
-my $OUT = $Name[0]."_Permuted.txt";
+my @Name = split(/\./, $hand1);
+my $OUT = $Name[0]."_DownSampled.txt";
 open(my $out, '>', $OUT) or die "Could not open file $OUT $!\n";
 #Print out final Matrix
+print $out "Name\tHit_Sequence\tTag_Sequence\tObserved_Counts\tProportion_of_Total_Counts\tWhole_Number_Proportion\t75%\t50%\t25%\t10%\t5%\n";
 foreach my $Fragment (@FragList){
 
     print $out $Final{$Fragment}."\n";
--- RCode.R
+++ RCode.R
@@ -17,6 +17,23 @@ CureMAST_d<-CureMAST_c[!is.na(CureMAST_c[,18]),]
 #Plot reproduibility
 cor(CureMAST_d[,9],CureMAST_d[,18])
 
+setwd("~/Dropbox/Sample_MPRA_Data/Down_Sampling/")
+setEPS()
+postscript("100Percent.eps")
+
+m <- rbind(c(1, 1), c(2, 3))
+print(m)
+layout(m)
+
+plot(CureMAST_d[,9], CureMAST_d[,18], pch =16, cex =.1, xlab="Replicate 1", ylab="Replicate 2", las=1, bty="n")
+abline(lm(CureMAST_d[,18]~CureMAST_d[,9]), col="red")
+mtext(cor(CureMAST_d[,9],CureMAST_d[,18]), side=3)
+
+hist(CureMAST_d[,9], breaks=200, las=1, bty="n", main="Rep 1", xlab="Fold Enrichment")
+hist(CureMAST_d[,18], breaks=200, las=1, bty="n",  main="Rep 2", xlab="Fold Enrichment")
+
+dev.off()
+
 setwd("~/Dropbox/Sample_MPRA_Data/")
 setEPS()
 postscript("Reproduce_WholeData.eps")
@@ -42,18 +59,18 @@ dev.off()
 #The downsampling script will use Col6 to seed an array to sample from with the correct probability for each tag
 
 Rep1_mRNA_COUNTS<-sum(Rep1_mRNA[,4])
-Rep1_mRNA_foo<-cbind(Rep1_mRNA[,1:4],Rep1_mRNA[,4]/sum(Rep1_mRNA[,4]),round(Rep1_mRNA[,4]/sum(Rep1_mRNA[,4])*1e10))
+Rep1_mRNA_foo<-cbind(Rep1_mRNA[,1:4],Rep1_mRNA[,4]/sum(Rep1_mRNA[,4]),round(Rep1_mRNA[,4]/sum(Rep1_mRNA[,4])*Rep1_mRNA_COUNTS))
 
 Rep1_pDNA_COUNTS<-sum(Rep1_pDNA[,4])
-Rep1_pDNA_foo<-cbind(Rep1_pDNA[,1:4],Rep1_pDNA[,4]/sum(Rep1_pDNA[,4]),round(Rep1_pDNA[,4]/sum(Rep1_pDNA[,4])*1e10))
+Rep1_pDNA_foo<-cbind(Rep1_pDNA[,1:4],Rep1_pDNA[,4]/sum(Rep1_pDNA[,4]),round(Rep1_pDNA[,4]/sum(Rep1_pDNA[,4])*Rep1_pDNA_COUNTS))
 
 
 Rep2_mRNA_COUNTS<-sum(Rep2_mRNA[,4])
-Rep2_mRNA_foo<-cbind(Rep2_mRNA[,1:4],Rep2_mRNA[,4]/sum(Rep2_mRNA[,4]),round(Rep2_mRNA[,4]/sum(Rep2_mRNA[,4])*1e10))
+Rep2_mRNA_foo<-cbind(Rep2_mRNA[,1:4],Rep2_mRNA[,4]/sum(Rep2_mRNA[,4]),round(Rep2_mRNA[,4]/sum(Rep2_mRNA[,4])*Rep2_mRNA_COUNTS))
 
 
 Rep2_pDNA_COUNTS<-sum(Rep2_pDNA[,4])
-Rep2_pDNA_foo<-cbind(Rep2_pDNA[,1:4],Rep2_pDNA[,4]/sum(Rep2_pDNA[,4]),round(Rep2_pDNA[,4]/sum(Rep2_pDNA[,4])*1e10))
+Rep2_pDNA_foo<-cbind(Rep2_pDNA[,1:4],Rep2_pDNA[,4]/sum(Rep2_pDNA[,4]),round(Rep2_pDNA[,4]/sum(Rep2_pDNA[,4])*Rep2_pDNA_COUNTS))
 
 ####Write XXXX_foo tables to file for input into perl downsampling script
 write.table(Rep1_mRNA_foo, file = "~/Dropbox/Sample_MPRA_Data/Down_Sampling/Rep1_mRNA.txt", sep = "\t", quote=F, row.names = FALSE, col.names = FALSE)
@@ -62,14 +79,59 @@ write.table(Rep1_pDNA_foo, file = "~/Dropbox/Sample_MPRA_Data/Down_Sampling/Rep1
 write.table(Rep2_mRNA_foo, file = "~/Dropbox/Sample_MPRA_Data/Down_Sampling/Rep2_mRNA.txt", sep = "\t", quote=F, row.names = FALSE, col.names = FALSE)
 write.table(Rep2_pDNA_foo, file = "~/Dropbox/Sample_MPRA_Data/Down_Sampling/Rep2_pDNA.txt", sep = "\t", quote=F, row.names = FALSE, col.names = FALSE)
 
+#########
+#Load the down sampled files
+Rep1_mRNA_DS<-read.table(file="Rep1_mRNA_DownSampled.txt", header=T, sep="\t")
+Rep1_pDNA_DS<-read.table(file="Rep1_pDNA_DownSampled.txt", header=T, sep="\t")
+
+Rep2_mRNA_DS<-read.table(file="Rep2_mRNA_DownSampled.txt", header=T, sep="\t")  
+Rep2_pDNA_DS<-read.table(file="Rep2_pDNA_DownSampled.txt", header=T, sep="\t")
+
+#Calc fold increase
+Rep1_DS<-data.frame(cbind(as.character(Rep1_mRNA_DS$Name), (Rep1_mRNA_DS[,6]/sum(Rep1_mRNA_DS[,6]))/(Rep1_pDNA_DS[,6]/sum(Rep1_pDNA_DS[,6])), (Rep1_mRNA_DS[,7]/sum(Rep1_mRNA_DS[,7]))/(Rep1_pDNA_DS[,7]/sum(Rep1_pDNA_DS[,7])), (Rep1_mRNA_DS[,8]/sum(Rep1_mRNA_DS[,8]))/(Rep1_pDNA_DS[,8]/sum(Rep1_pDNA_DS[,8])), (Rep1_mRNA_DS[,9]/sum(Rep1_mRNA_DS[,9]))/(Rep1_pDNA_DS[,9]/sum(Rep1_pDNA_DS[,9])), (Rep1_mRNA_DS[,10]/sum(Rep1_mRNA_DS[,10]))/(Rep1_pDNA_DS[,10]/sum(Rep1_pDNA_DS[,10])), (Rep1_mRNA_DS[,11]/sum(Rep1_mRNA_DS[,11]))/(Rep1_pDNA_DS[,11]/sum(Rep1_pDNA_DS[,11]))))
+Rep2_DS<-data.frame(cbind(as.character(Rep2_mRNA_DS$Name), (Rep2_mRNA_DS[,6]/sum(Rep2_mRNA_DS[,6]))/(Rep2_pDNA_DS[,6]/sum(Rep2_pDNA_DS[,6])), (Rep2_mRNA_DS[,7]/sum(Rep2_mRNA_DS[,7]))/(Rep2_pDNA_DS[,7]/sum(Rep2_pDNA_DS[,7])), (Rep2_mRNA_DS[,8]/sum(Rep2_mRNA_DS[,8]))/(Rep2_pDNA_DS[,8]/sum(Rep2_pDNA_DS[,8])), (Rep2_mRNA_DS[,9]/sum(Rep2_mRNA_DS[,9]))/(Rep2_pDNA_DS[,9]/sum(Rep2_pDNA_DS[,9])), (Rep2_mRNA_DS[,10]/sum(Rep2_mRNA_DS[,10]))/(Rep2_pDNA_DS[,10]/sum(Rep2_pDNA_DS[,10])), (Rep2_mRNA_DS[,11]/sum(Rep2_mRNA_DS[,11]))/(Rep2_pDNA_DS[,11]/sum(Rep2_pDNA_DS[,11]))))
+
+#Table Specific Down sampling
+HUNPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,2]), as.character(Rep2_DS[,2])))
+STFPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,3]), as.character(Rep2_DS[,3])))
+FIFPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,4]), as.character(Rep2_DS[,4])))
+TWFPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,5]), as.character(Rep2_DS[,5])))
+TENPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,6]), as.character(Rep2_DS[,6])))
+FIVPercent<-data.frame(cbind(as.character(Rep1_DS[,1]), as.character(Rep1_DS[,7]), as.character(Rep2_DS[,7])))
+
+#Clean Each DS for ploting
+CleanDS <- function(FRAME){
+  FRAME<-FRAME[FRAME[,2] != Inf ,]
+  FRAME<-FRAME[FRAME[,3] != Inf ,]
+  FRAME<-FRAME[!is.na(FRAME[,2]),]
+  FRAME<-FRAME[!is.na(FRAME[,3]),]
+  return(FRAME)
+}
+
+HUNPercent<-CleanDS(HUNPercent)
+STFPercent<-CleanDS(STFPercent)
+FIFPercent<-CleanDS(FIFPercent)
+TWFPercent<-CleanDS(TWFPercent)
+TENPercent<-CleanDS(TENPercent)
+FIVPercent<-CleanDS(FIVPercent)
 
 
+setwd("~/Dropbox/Sample_MPRA_Data/Down_Sampling/")
+setEPS()
+postscript("100Percent.eps")
 
+m <- rbind(c(1, 1), c(2, 3))
+print(m)
+layout(m)
 
+plot(HUNPercent[,2], HUNPercent[,3], pch =16, cex =.1, xlab="Replicate 1", ylab="Replicate 2", las=1, bty="n")
+abline(lm(HUNPercent[,3]~HUNPercent[,2]), col="red")
+mtext(cor(HUNPercent[,2],HUNPercent[,3]), side=3)
 
+hist(CureMAST_d[,9], breaks=200, las=1, bty="n", main="Rep 1", xlab="Fold Enrichment")
+hist(CureMAST_d[,18], breaks=200, las=1, bty="n",  main="Rep 2", xlab="Fold Enrichment")
 
-
-
+dev.off()
 
 
 
